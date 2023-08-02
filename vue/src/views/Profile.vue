831ebcb6f12df797599bf1de7501b72b4cf11f43
levintoo <tooklevin@gmail.com>
2023-08-02 23:33:22 +0300

    vue profile page

--- vue/src/views/Profile.vue
+++ vue/src/views/Profile.vue
@@ -1,1 +1,80 @@
-<template></template>
+<script setup>
+import AuthenticatedLayout from '@/layouts/AuthenticatedLayout.vue'
+import InputError from '@/components/InputError.vue';
+import InputLabel from '@/components/InputLabel.vue';
+import PrimaryButton from '@/components/PrimaryButton.vue';
+import TextInput from '@/components/TextInput.vue';
+import {ref} from "vue";
+
+const form = ref({
+    name: "",
+    email: "",
+});
+</script>
+<template>
+    <AuthenticatedLayout>
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>
+        </div>
+    </header>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+
+                    <header>
+                        <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
+
+                        <p class="mt-1 text-sm text-gray-600">
+                            Update your account's profile information and email address.
+                        </p>
+                    </header>
+
+                    <form class="mt-6 space-y-6">
+                        <div>
+                            <InputLabel for="name" value="Name" />
+
+                            <TextInput
+                                id="name"
+                                type="text"
+                                class="mt-1 block w-full"
+                                v-model="form.name"
+                                required
+                                autofocus
+                                autocomplete="name"
+                            />
+
+                            <InputError class="mt-2" :message="form.name" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="email" value="Email" />
+
+                            <TextInput
+                                id="email"
+                                type="email"
+                                class="mt-1 block w-full"
+                                v-model="form.email"
+                                required
+                                autocomplete="email"
+                            />
+
+                            <InputError class="mt-2" :message="form.email" />
+                        </div>
+
+                        <div class="flex items-center gap-4">
+                            <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+
+                            <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" class="transition ease-in-out">
+                                <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                            </Transition>
+                        </div>
+                    </form>
+
+            </div>
+
+        </div>
+    </div>
+    </AuthenticatedLayout>
+</template>
